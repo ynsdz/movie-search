ea8ae1a531201bae55f4b602af0cb02bed609bb3
Yunus Emre Dize <yunusemredize@gmail.com>
2024-08-14 17:04:46 +0300

    unnecessary commend line removed

--- app/search/components/movies/movie-card/index.tsx
+++ app/search/components/movies/movie-card/index.tsx
@@ -124,29 +124,3 @@ function MovieCard(props: Props) {
 }
 
 export default MovieCard;
-
-/**
- 
-
-function getHighlightedText(text: string, highlight: string) {
-  // Split on highlight term and include term into parts, ignore case
-  const parts = text.split(new RegExp(`(${highlight})`, "gi"));
-  return (
-    <span>
-      {" "}
-      {parts.map((part, i) => (
-        <span
-          key={i}
-          style={
-            part.toLowerCase() === highlight.toLowerCase()
-              ? { fontWeight: "bold" }
-              : {}
-          }
-        >
-          {part}
-        </span>
-      ))}{" "}
-    </span>
-  );
-}
- */
